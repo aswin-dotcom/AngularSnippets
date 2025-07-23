631ce5108383c5875947e6b7eccb8db3b2190f83
aswin-dotcom <aswin42samuel7@gmail.com>
2025-07-23 22:21:18 +0530

    Form Array added in reactive form

--- reactive/src/app/app.component.ts
+++ reactive/src/app/app.component.ts
@@ -1,31 +1,35 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'reactive';
-  reactiveForm:FormGroup;
-  ngOnInit()
-  {
+  reactiveForm: FormGroup;
+  ngOnInit() {
     this.reactiveForm = new FormGroup({
-      firstname :  new FormControl(null,Validators.required),
-      lastname : new FormControl(null, Validators.required),
-      email: new FormControl(null,[Validators.required,Validators.email]),
+      firstname: new FormControl(null, Validators.required),
+      lastname: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       address: new FormGroup({
         street: new FormControl(null, [Validators.required]),
-        pincode: new FormControl(null,[Validators.required,Validators.maxLength(6)])
-      })
-
-    })
+        pincode: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(6),
+        ]),
+      }),
+      skills:new FormArray([
+        new FormControl(null,Validators.required),
+        new FormControl(null,Validators.required),
+        new FormControl(null,Validators.required)
+      ])
+    });
   }
-  handlesubmit()
-  {
+  handlesubmit() {
     console.log(this.reactiveForm);
   }
-
 }
