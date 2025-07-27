615591212b7c3628a3625e47497074b4f33b4d04
aswin-dotcom <aswin42samuel7@gmail.com>
2025-07-27 12:18:07 +0530

    SetValue in reactive Forms

---
+++ setandpatch/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'setandpatch';
+
+  reactiveForms: FormGroup;
+  user:string;
+
+  ngOnInit() {
+    this.reactiveForms = new FormGroup({
+      Firstname: new FormControl(null, Validators.required),
+      lastname: new FormControl(null, Validators.required),
+      username: new FormControl(''),
+    });
+  }
+
+  handleUsername() {
+    this.reactiveForms.setValue({
+      Firstname: this.reactiveForms.get('Firstname').value,
+      lastname:this.reactiveForms.get('lastname').value,
+      username: this.getusername()
+    });
+  }
+  getusername(){
+    let date =  new Date();
+
+   this.user =  this.reactiveForms.get('Firstname').value.slice(0,3)+ this.reactiveForms.get('lastname').value.slice(0,3)+date.getFullYear();
+   return this.user;
+  }
+}
