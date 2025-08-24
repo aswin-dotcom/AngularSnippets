d8b3d3aa4d3edadfd52f826bba00b1600a25d4f0
aswin-dotcom <aswin42samuel7@gmail.com>
2025-08-24 12:21:13 +0530

    Global routing for module added

--- Angular-module/src/app/app-routing.module.ts
+++ Angular-module/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StatComponent } from './dashboard/stat/stat.component';
+import { TaskComponent } from './dashboard/task/task.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: 'stat', component: StatComponent },
+      { path: 'task', component: TaskComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular-module/src/app/dashboard/DashBoard.module.ts
+++ Angular-module/src/app/dashboard/DashBoard.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from "@angular/core";
 import { DashboardComponent } from "./dashboard.component";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "../Shared.module";
+import { TaskComponent } from "./task/task.component";
+import { StatComponent } from './stat/stat.component';
+import { AppRoutingModule } from "../app-routing.module";
+import { RouterModule } from "@angular/router";
+// import { TaskComponent } fro./task/task.componentent';
 
 @NgModule({
   declarations:[
-    DashboardComponent
+    DashboardComponent,
+    TaskComponent,
+    StatComponent
   ],
   exports:[
     DashboardComponent,
     SharedModule
   ],
-  imports:[
+  imports: [
     CommonModule,
-    SharedModule
-  ]
+    SharedModule,
+    AppRoutingModule,
+    RouterModule
+]
 })
 export class DashboardModule{}
