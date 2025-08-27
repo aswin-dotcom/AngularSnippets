71e4cfeee2bf9871f780e186e6ea6b737b0b9453
aswin-dotcom <aswin42samuel7@gmail.com>
2025-08-27 10:46:50 +0530

    computed value and effect

--- signals/src/app/app.ts
+++ signals/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,12 @@ import { Component, signal } from '@angular/core';
 export class App {
   protected readonly title = signal('signals');
   counter = signal(0);
+  doublecounter = computed(()=>this.counter()*2);
   message = signal<any[]>([]);
-  // increamet() {
-  //   this.counter.set(this.counter() + 1);
-  // }
+  constructor()
+  {
+    effect(()=>console.log(` effect called `))
+  }
   increamet() {
     this.counter.update((val) => val + 1);
     this.message.update((pre) => [...pre, this.counter()]);
